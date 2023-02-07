7abab84600df3458fa5eb4521c868b0c8315ebfb
Bhargav Kakadiya <kakadiyabhargav7@gmail.com>
2023-02-07 13:46:51 -0800

    add chain to create workflow of llm + prompt

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from langchain.llms import OpenAI
-
 from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
+
+llm = OpenAI(temperature=0.9)
 prompt = PromptTemplate(
     input_variables=["product"],
     template="What is a good name for a company that makes {product}?",
 )
 
-llm = OpenAI(temperature=0.9)
+chain = LLMChain(llm=llm, prompt=prompt)
 
-print(llm(prompt.format(product="colorful socks")))
+result = chain.run("coloring book")
+
+print(result)
